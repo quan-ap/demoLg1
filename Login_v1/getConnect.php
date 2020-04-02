26d516732a64f0345d43315c8c1dc8ae352273e0
quansupermaket <62633212+quansupermaket@users.noreply.github.com>
2020-04-02 22:52:58 +0700

    Sơn vất vả làm
    Đcm quân

--- Login_v1/getConnect.php
+++ Login_v1/getConnect.php
@@ -1,6 +1,6 @@
 <?php
 	$connection_string= "host=ec2-54-197-48-79.compute-1.amazonaws.com port=5432 dbname=d122repi78kaui user=ggsohzyyzfnwpc password=4e756f3562eb875e991c09ddb68ced9f39d8757c77355e8965c4ef93520b749f ";
-	$conn=pg_connect(connection_string);
+	$conn=pg_connect($connection_string);
 	if(!$conn){
 		echo "đm đ kết nối đc";
 	}else{
